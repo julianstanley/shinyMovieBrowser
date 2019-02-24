52d0258e833a491f8a34c77a95fbd39c2e10c34d
Julian A. Stanley <stanley.ju@husky.neu.edu>
2019-02-24 13:50:32 -0500

    Added some extra graphs

--- app.R
+++ app.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(ggplot2)
+library(dplyr)
+library(DT)
 load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_4850/datasets/movies.Rdata"))
 
 # Define UI for application that plots features of movies 
@@ -31,12 +33,28 @@ ui <- fluidPage(
                                     "MPAA rating" = "mpaa_rating", 
                                     "Critics rating" = "critics_rating",
                                     "Audience rating" = "audience_rating"),
-                        selected = "mpaa_rating")
+                        selected = "mpaa_rating"),
+            
+            # Set alpha level
+            sliderInput(inputId = "alpha",
+                        label = "Alpha:", 
+                        min = 0, max = 1, 
+                        value = 0.5),
+            
+            # Show dat table
+            checkboxInput(inputId = "show_data",
+                          label = "Show data table",
+                          value = TRUE)
         ),
         
         # Outputs
         mainPanel(
-            plotOutput(outputId = "scatterplot")
+            # Show the plots
+            plotOutput(outputId = "scatterplot"),
+            plotOutput(outputId = "densityplot", height = 200),
+            
+            # Show the data table
+            DT::dataTableOutput(outputId = "moviestable")
         )
     )
 )
@@ -48,7 +66,22 @@ server <- function(input, output) {
     output$scatterplot <- renderPlot({
         ggplot(data = movies, aes_string(x = input$x, y = input$y,
                color = input$z)) +
-            geom_point()
+            geom_point(alpha = input$alpha)
+    })
+    
+    # create densityplot
+    output$densityplot <- renderPlot({
+        ggplot(data = movies, aes_string(x = input$x)) +
+            geom_density(alpha = input$alpha)
+    })
+    
+    # Print data table if checked
+    output$moviestable <- DT::renderDataTable({
+        if(input$show_data){
+            DT::datatable(data = movies %>% select(1:7),
+                          options = list(pageLength = 10),
+                          rownames = FALSE)
+        }
     })
 }
 
